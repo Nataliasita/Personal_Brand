8f424e62a2f2e75f62f304da76dabcf88cc4cdec
Natalia Gonzalez <nataliasita04@gmail.com>
2021-02-24 10:53:11 -0500

    archivos js migrados replit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 import validator from './validator.js';
 
-console.log(validator);
+cambio_paginas();
+function cambio_paginas(){
+let comprar = document.getElementById("comprar"); 
+comprar.onclick= function(){
+  document.querySelector(".header").style.display = "none";
+  document.querySelector(".seleccion").style.display = "block"; 
+}
+
+let carrito1 = document.getElementById("carrito1"); 
+carrito1.onclick= function(){
+  document.querySelector(".seleccion").style.display = "none";
+  document.querySelector(".tarjeta").style.display = "block"; 
+  document.getElementById("detalle").textContent = "Paleta Sombras";
+  document.getElementById("valor").textContent = "$350.000";
+  document.getElementById("image1").style.display = "block";
+  document.getElementById("final").textContent = "$350.000"; 
+}
+
+let carrito2 = document.getElementById("carrito2");  carrito2.onclick= function(){
+  document.querySelector(".seleccion").style.display = "none";
+  document.querySelector(".tarjeta").style.display = "block"; 
+  document.getElementById("detalle").textContent = "Set Brochas";
+  document.getElementById("valor").textContent = "$200.000";
+  document.getElementById("image2").style.display = "block"; 
+  document.getElementById("final").textContent = "$200.000";
+}
+
+let carrito3 = document.getElementById("carrito3");  
+carrito3.onclick= function(){
+  document.querySelector(".seleccion").style.display = "none";
+  document.querySelector(".tarjeta").style.display = "block"; 
+  document.getElementById("detalle").textContent = "Set Labiales";
+  document.getElementById("valor").textContent = "$125.000";
+  document.getElementById("image3").style.display = "block";
+  document.getElementById("final").textContent = "$125.000"; 
+}
+let validar = document.getElementById("validar"); 
+validar.onclick= function(){
+  document.querySelector(".tarjeta").style.display = "none";
+  document.querySelector(".verificacion").style.display = "block"; 
+  let numerosIsValid = document.getElementById("numerotc").value.split("");   
+
+ let numerosMaskify = document.getElementById("numerotc").value.split(""); 
+  validator.isValid(numerosIsValid);
+  validator.maskify(numerosMaskify);
+/*console.log(validator.resultado);
+  if (validator.resultado === true)
+  {
+     document.getElementById("ver1").textContent = "aceptado";
+  }
+  else{
+    document.getElementById("ver1").textContent = "rechazado";
+  }*/
+}
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,100 @@
 const validator = {
   // ...
+ isValid,
+ maskify,
 };
-
 export default validator;
+
+function isValid(numerosIsValid){
+  let reversa = numerosIsValid.reverse().map(function(item) { return parseInt(item, 10); });
+  //let x =typeof(nuevo[3]);
+  console.log(reversa);
+
+  for (let i=1; i<reversa.length; i=i+2) {
+        reversa[i]=reversa[i]*2;
+  }
+  console.log(reversa);
+  
+  let string_array=reversa.toString().split(",");
+  console.log(string_array);
+  
+  for (let i=0;i<string_array.length;i++){
+        if (string_array[i] > 9){
+            let numeros_dobles = string_array[i].split("").map(function(item) { return parseInt(item, 10); });
+            console.log(numeros_dobles);
+            string_array[i]=numeros_dobles[0]+numeros_dobles[1];
+        }
+  }
+  let number_array = string_array.map(function(item) { return parseInt(item, 10); });
+ console.log(number_array);
+
+ let suma_final=0;
+  for (let i=0;i<number_array.length; i++){
+       suma_final=suma_final+number_array[i];
+  }
+  console.log(suma_final);
+  suma_final=suma_final % 10;
+  console.log(suma_final);
+  let resultado;
+  if(suma_final === 0)  {  resultado=true;}
+  else { resultado= false;}
+  console.log(resultado);
+}
+
+function maskify(numerosMaskify){
+  console.log(numerosMaskify);
+  //let borrar = numerosMaskify.slice(-4).join("");
+  //console.log(borrar);
+  let mascara= numerosMaskify;
+  for(let i=0;i<(mascara.length-4);i++){
+    mascara[i]="#";
+  }
+  console.log(mascara.join(""));
+}
+
+/*
+// importamos el objeto `validator`, que contiene las funciones `isValid` y `maskify`
+import validator from '../src/validator';
+
+describe('validator', () => {
+  it('debería ser un objeto', () => {
+    expect(typeof validator).toBe('object');
+  });
+
+  describe('validator.isValid', () => {
+    it('debería ser una función', () => {
+      expect(typeof validator.isValid).toBe('function');
+    });
+
+    it.skip('debería retornar true para "4083952015263"', () => {
+      // escribe aquí tu test
+    });
+
+    it.skip('debería retornar true para "79927398713"', () => {
+      // escribe aquí tu test
+    });
+
+    it.skip('debería retornar false para "1234567890"', () => {
+      // escribe aquí tu test
+    });
+  });
+
+  describe('validator.maskify', () => {
+    it('debería ser una función', () => {
+      expect(typeof validator.maskify).toBe('function');
+    });
+
+    it.skip('Debería retornar "############5616" para "4556364607935616"', () => {
+      // escribe aquí tu test
+    });
+
+    it.skip('Debería retornar "1" para "1"', () => {
+      // escribe aquí tu test
+    });
+
+    it.skip('Debería retornar "######orld" para "helloworld"', () => {
+      // escribe aquí tu test
+    });
+  });
+});
+*/
